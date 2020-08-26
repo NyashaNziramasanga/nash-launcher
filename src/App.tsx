efe40df3575987f0f1b58331b7f7629572463551
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-08-26 17:21:53 +0930

    Rearraged icons

--- src/App.tsx
+++ src/App.tsx
@@ -18,12 +18,12 @@ export default function App() {
       <Content content={'Quick link access for Nash content'} />
       <div className='modal-icons'>
         <div className='flex-container'>
-          <Icon url={Data.facebook.url} icon={Data.facebook.icon} />
-          <Icon url={Data.github.url} icon={Data.github.icon} />
-          <Icon url={Data.instagram.url} icon={Data.instagram.icon} />
-          <Icon url={Data.linkedin.url} icon={Data.linkedin.icon} />
-          <Icon url={Data.twitter.url} icon={Data.twitter.icon} />
           <Icon url={Data.website.url} icon={Data.website.icon} />
+          <Icon url={Data.linkedin.url} icon={Data.linkedin.icon} />
+          <Icon url={Data.github.url} icon={Data.github.icon} />
+          <Icon url={Data.facebook.url} icon={Data.facebook.icon} />
+          <Icon url={Data.instagram.url} icon={Data.instagram.icon} />
+          <Icon url={Data.twitter.url} icon={Data.twitter.icon} />
         </div>
       </div>
     </div>
