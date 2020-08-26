a9b24c9245a8e506a73da290be13c01701304937
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-08-26 19:10:18 +0930

    Added build script

---
+++ dist/precache-manifest.ed1d49b6abd6c850a82e58c7330bf59b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0d874de85e490b3dcec121a69eafee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b7fc08f381b4efdf007",
+    "url": "/static/css/main.5f2024f2.chunk.css"
+  },
+  {
+    "revision": "52a0b4aad51aa3abeaa8",
+    "url": "/static/js/2.974946c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.974946c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7fc08f381b4efdf007",
+    "url": "/static/js/main.ef1d458c.chunk.js"
+  },
+  {
+    "revision": "c89ddfb41e9b51594fe7",
+    "url": "/static/js/runtime-main.0f9b9013.js"
+  }
+]);
